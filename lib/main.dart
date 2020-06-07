42bc01cb5b10224a280071cf81bafdebea8110e4
pratikrana1998 <pratikrana1998@users.noreply.github.com>
2020-06-07 19:50:13 +0530

    adding sounds to the app and playing notes using audioplayers package and setting up the UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int sNum) {
+    final player = AudioCache();
+    player.play('note$sNum.wav');
+  }
+
+  Expanded buildKey({ Color color, int sNum }) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(sNum);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+             children: <Widget>[
+               buildKey(color: Colors.purple, sNum: 1),
+               buildKey(color: Colors.indigo, sNum: 2),
+               buildKey(color: Colors.teal, sNum: 3),
+               buildKey(color: Colors.green, sNum: 4),
+               buildKey(color: Colors.yellow, sNum: 5),
+               buildKey(color: Colors.orange, sNum: 6),
+               buildKey(color: Colors.red, sNum: 7),
+             ],
+          ),
         ),
       ),
     );
